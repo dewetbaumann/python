4b1e16926715c469045a59a0f308d965a38a8790
De Wet Baumann <49966304+dewetbaumann@users.noreply.github.com>
2021-06-12 22:26:03 -0300

    Problema 4
    - Correcciones del problema 3
    - Problema 4 resuelto

--- problem 3/problem3.py
+++ problem 3/problem3.py
@@ -3,54 +3,59 @@ from os import system as console
 console('cls')
 console('color 0b')
 
-disapproved = 0
-add = 0
-aux = True
+def gradeInput():
+    disapproved = 0
+    addGrade = 0
+    aux = True
+    for i in range(6):
+        if disapproved > 1:
+            break
+        else:
+            while (aux):
+                try:
+                    grade = float(input(f'Ingrese la nota del parcial({i+1}): '))
+                    if grade > 0 and grade < 6:
+                        disapproved += 1
+                        aux = False
+                    
+                    elif grade >= 6 and grade <= 10:
+                        addGrade += grade            
+                        aux = False
 
-for i in range(6):
-    if disapproved > 1:
-        break
-    else:
+                    else:
+                        print('La nota no puede ser menor a 0 y mayor a 10')
+
+                except:
+                    print('Error: La nota sola puede ser un racional entre 0 y 10')
+
+            aux = True
+    
+    return addGrade,disapproved
+
+def gradeValidation(addGrade,disapproved):
+    if disapproved == 0:
+        average = addGrade/6
+        print (f'Promedio: {average:.2f}')
+
+    elif disapproved == 1:
         while (aux):
             try:
-                grade = float(input(f'Ingrese la nota del parcial({i+1}): '))
-                if grade > 0 and grade < 6:
-                    disapproved += 1
-                    aux = False
+                grade = float(input(f'Ingrese la nota del parcial recuperatorio: '))
                 
-                elif grade >= 6 and grade <= 10:
-                    add += grade            
-                    aux = False
-
-                else:
-                    print('La nota no puede ser menor a 0 y mayor a 10')
+                if grade > 0 and grade < 7:
+                    print('Perdiste la materia por desaprobar 2 parciales')
+                
+                elif grade > 7 and grade <= 10:
+                    average = addGrade/7
+                    print (f'Aprobado con un promedio de:{average:.2f}\ny con un parcial desaprobado!')
+                    
+                aux = False
 
             except:
-                print('Error: La nota sola puede ser un racional entre 0 y 10')
+                print('Error: La nota sola puede ser un racional')
+        
+    elif disapproved == 2:
+        print('Perdiste la materia por desaprobar 2 parciales')
 
-        aux = True
-
-if disapproved == 0:
-    average = add/6
-    print (f'Promedio: {average:.2f}')
-
-elif disapproved == 1:
-    while (aux):
-        try:
-            grade = float(input(f'Ingrese la nota del parcial recuperatorio: '))
-            
-            if grade > 0 and grade < 7:
-                print('Perdiste la materia por desaprobar 2 parciales')
-            
-            elif grade > 7 and grade <= 10:
-                average = add/7
-                print (f'Aprobado con un promedio de:{average:.2f}\ny con un parcial desaprobado!')
-                
-            aux = False
-
-        except:
-            print('Error: La nota sola puede ser un racional')
-    
-elif disapproved == 2:
-    print('Perdiste la materia por desaprobar 2 parciales')
-
+addGrade,disapproved = gradeInput
+gradeValidation(addGrade,disapproved)
---
+++ problem 4/problem4.py
@@ -0,0 +1,18 @@
+from os import system as console
+
+console('cls')
+console('color 0b')
+
+mes = input('ingrese el mes: ').lower()
+
+if mes == 'abril' or mes == 'junio' or mes == 'septiembre'or mes == 'noviembre':
+    print ('El mes trae 30 dias')
+
+elif mes == 'enero' or mes == 'marzo' or mes == 'mayo' or mes == 'julio' or mes == 'agosto' or mes == 'octubre' or mes == 'diciembre':
+    print ('El mes trae 31 dias')
+
+elif mes == 'febrero':
+    print ('El mes trae 28 o 29 dias, todo un misterio la verdad esto de los 100tificos')
+
+else:
+    print('verifica la escritura del mes')
